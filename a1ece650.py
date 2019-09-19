8dccde3bddaec34daec6dd773591f4bdd55e7596
Rongzhi <gurongzhi1997@gmail.com>
2019-09-19 00:08:52 -0400

    Add line parser to read street info from stdin

--- a1ece650.py
+++ a1ece650.py
@@ -1,10 +1,46 @@
-import sys
+"""
+Rongzhi Gu
+20855042
 
-# YOUR CODE GOES HERE
+test example:
+a "Weber Street" (2,-1) (2,2) (5,5) (5,6) (3,8)
+
+9-18
+Error1:line overlap
+"""
+import sys
+import re
+
+# store street info as a dictionary
+st = {}
+
+
+def add_street(_name, _coord):
+    global st
+    if _name[0] not in st:
+        st[_name[0]] = []
+    else:
+        print 'Error: %s specified for a street that has been added. Try using option c to change it' % _name
+        return False
+    for i in _coord:
+        st[_name[0]].append(tuple(eval(i)))
+    return True
+
+def parse_line(_line):
+    # _line = 'a "Weber Street" (2,-1) (2,2) (5,5) (5,6) (3,8)'
+    if _line[0] == 'a':
+        name = re.findall(r'\"(.*)\"', _line)
+        coord = re.findall(r'\(.*?\)', _line)
+        # print name
+        # print coord
+        if not add_street(name, coord):
+            return
+    # print _line
+    return
 
 def main():
     ### YOUR MAIN CODE GOES HERE
-
+    global st
     ### sample code to read from stdin.
     ### make sure to remove all spurious print statements as required
     ### by the assignment
@@ -12,11 +48,12 @@ def main():
         line = sys.stdin.readline()
         if line == '':
             break
-        print 'read a line:', line
-
-    print 'Finished reading input'
+        parse_line(line)
+        print st
+    # print 'Finished reading input'
     # return exit code 0 on successful termination
     sys.exit(0)
+
 
 if __name__ == '__main__':
     main()
