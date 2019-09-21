45466d0b8a9327ce3281066bdc2eef891764e66f
Rongzhi <gurongzhi1997@gmail.com>
2019-09-21 04:54:28 -0400

    Add error output, move cmd to main func

--- Class.py
+++ Class.py
@@ -1,17 +1,27 @@
 class StreetDatabase(object):
     def __init__(self):
         # store street info in a dictionary
-        self.st_info = {}
+        self.st_data = {}
 
     def add(self, name, coord):
-        if name[0] not in self.st_info:
-            self.st_info[name[0]] = []
+        if name[0].lower() not in [current_st.lower() for current_st in self.st_data]:
+            self.st_data[name[0]] = []
+            for i in coord:
+                self.st_data[name[0]].append(tuple(eval(i)))
         else:
-            print('Error: "%s" specified for a street that has been added. Try option c to change it' % name[0])
-            return False
-        for i in coord:
-            self.st_info[name[0]].append(tuple(eval(i)))
-        return True
+            raise Exception('`a` specified for a street that has been added. Try option `c` to change it')
+
+    def change(self, name, coord):
+        if name[0].lower() not in [current_st.lower() for current_st in self.st_data]:
+            raise Exception('`c` specified for a street that has not been added. Try option `a` to add it')
+        else:
+            for current_st in self.st_data:
+                if name[0].lower() == current_st.lower():
+                    self.st_data[current_st] = []
+                    for i in coord:
+                        self.st_data[current_st].append(tuple(eval(i)))
+                    break
 
     def check_street(self):
-        print(self.st_info)
+        print(self.st_data)
+
--- a1ece650.py
+++ a1ece650.py
@@ -5,7 +5,13 @@ Rongzhi Gu
 test example:
 a "Weber Street" (2,-1) (2,2) (5,5) (5,6) (3,8)
 a "King Street S" (4,2) (4,8)
+c "KING STREET S" (1,2)
 a "Davenport Road" (1,4) (5,8)
+a "Weber Street" 1,2,3,4,5,6
+a "Weber Street" (1,2 (3,4) (5,6)
+a "Weber Street" (1,2)(3,4) (5,6)
+a"Weber Street" (1,2) (3,4) (5,6)
+a "Weber Street"(1,2) (3,4) (5,6)
 g
 
 9-18
@@ -17,42 +23,52 @@ import re
 from Class import *
 
 
-def command_parser(_line, _st_info):
-    # option a for adding a street
-    if _line[0] == 'a' and _line[1] == ' ':
+def command_parser(_line):
+    arg_list = _line.split('"')
+    if (_line[0] == 'a' or _line[0] == 'c') and _line[1] == ' ':
         name = re.findall(r'\"(.*)\"', _line)
         coord = re.findall(r'\(.*?\)', _line)
-        _st_info.add(name, coord)
-        _st_info.check_street()
-        # print name
-        # print coord
-        return
-    # option g for generating graph
-    if _line[0] == 'g' and _line[1] == ' ':
-        pass
-
-    # option r for removing a street
-    if _line[0] == 'r' and _line[1] == ' ':
-        pass
-
-    # option c for changing street info
-    if _line[0] == 'c' and _line[1] == ' ':
-        pass
-    # print _line
-
-    # if not anyone above, return error meg
-    print('Error: error input, try one of the options "a, c, r, g"')
-    return
-
+        if len(name) == 1 and len(arg_list) == 3 and arg_list[2][0] == ' ' and coord != []:
+            for i in coord:
+                if len(tuple(eval(i))) != 2:
+                    raise Exception("wrong coordinates")
+        else:
+            raise Exception("wrong argument number for option `%c`" % _line[0])
+        return _line[0], name, coord
+    else:
+        raise Exception("command unknown, try one of the options `a, c, r, g`")
 
 def main():
-    # YOUR MAIN CODE GOES HERE
-    st_info = StreetDatabase()
+    st_data = StreetDatabase()
     while True:
-        line = sys.stdin.readline()
-        if line == '':
-            break
-        command_parser(line, st_info)
+        try:
+            line = sys.stdin.readline()
+            if line == '':
+                break
+            cmd = command_parser(line)
+
+            # option a for adding a street
+            if cmd[0] == 'a':
+                st_data.add(cmd[1], cmd[2])
+
+            # option c for adding a street
+            elif cmd[0] == 'c':
+                st_data.change(cmd[1], cmd[2])
+
+            # option g for generating graph
+            elif cmd[0] == 'g':
+                pass
+
+            # option r for removing a street
+            elif cmd[0] == 'r' :
+                pass
+
+        except SyntaxError:
+            sys.stderr.write("Error: wrong coordinates\n")
+        except Exception as exp:
+            sys.stderr.write("Error: " + str(exp) + '\n')
+
+        st_data.check_street()
     # print 'Finished reading input'
     # return exit code 0 on successful termination
     sys.exit(0)
